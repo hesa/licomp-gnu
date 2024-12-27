ee4d5d65d966bc3f13a44b5604c29792f8177b48
Henrik Sandklef <hesa@sandklef.com>
2024-12-27 16:48:06 +0100

    add python files

---
+++ licomp_gnuguide/config.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+licomp_gnuguide_version = '0.3.0'
+my_supported_api_version = '0.3'
+
+cli_name = 'licomp-gnuguide'
+module_name = 'licomp_gnuguide'
+
+disclaimer = ''
+
+description = """
+Simple command line tool to check compatibility between two licenses, given context
+"""
+
+epilog = """
+
+AUTHOR
+  Henrik Sandklef
+
+PROJECT SITE
+  https://github.com/hesa/licomp-gnuguide
+
+REPORTING BUGS
+  File a ticket at https://github.com/hesa/licomp-gnuguide/issues
+
+COPYRIGHT
+  Copyright (c) 2024 Henrik Sandklef<hesa@sandklef.com>.
+  License GPL-3.0-or-later
+
+ATTRIBUTION
+  licomp-gnuguide is using the license compatibility graph from
+
+
+"""
