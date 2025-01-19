7ca798e52964e2ad84723a7a3f2a09164d641df9
hesa <hesa@localhost>
2025-01-19 10:02:48 +0000

    Release 0.5.0

--- licomp_gnuguide/config.py
+++ licomp_gnuguide/config.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-licomp_gnuguide_version = '0.4.0'
+licomp_gnuguide_version = '0.5.0'
 my_supported_api_version = '0.5'
 
 cli_name = 'licomp-gnuguide'
